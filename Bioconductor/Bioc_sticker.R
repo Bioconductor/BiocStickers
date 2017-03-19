0816452399c19d169bc731d273d755e6b2ed6375
Laurent <lg390@cam.ac.uk>
2017-03-19 22:00:17 +0000

    Bioconductor sticker

---
+++ Bioconductor/Bioc_sticker.R
@@ -0,0 +1,14 @@
+library("hexSticker")
+## GithubSHA1: 89e4d60a370f0457a0c4c08a95d4065e29275809
+
+bioc_blue <- "#1892AA"
+bioc_green <- "#99C53C"
+
+sticker("biocnote.png", package = "Bioconductor",
+        p_size = 16,
+        p_y = 1.47,
+        p_color = bioc_blue,
+        s_x = 1.25, s_y = 0.8,
+        s_width = 0.55, s_height = 0.55,
+        h_color = bioc_blue,
+        h_fill = "#FFFFFF")
---
+++ Bioconductor/README.md
@@ -0,0 +1,10 @@
+# The `MSnbase` package sticker
+
+* Project URL: http://bioconductor.org/
+* Maintainer: [Laurent Gatto](https://github.com/lgatto/)
+* License: Creative Commons Attribution
+  [CC-BY](https://creativecommons.org/licenses/by/2.0/). Feel free to
+  share and adapt, but don't forget to credit the author.
+
+<img src="./Bioconductor.png" height="200">
+
