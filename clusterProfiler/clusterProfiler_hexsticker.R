42d5847e9379851245c30d83a5fc5284160d504d
guangchuang yu <guangchuangyu@gamil.com>
2017-07-25 19:43:07 +0800

    clusterProfiler

--- clusterProfiler/clusterProfiler_hexsticker.R
+++ clusterProfiler/clusterProfiler_hexsticker.R
@@ -5,4 +5,4 @@ data(gcSample)
 y = compareCluster(gcSample, 'enrichDO')
 p <- dotplot(y) + theme_void() + theme(legend.position='none')+ theme_transparent()  +
     theme(panel.grid.major = element_line(size=.05, color='grey'))
-sticker(p+scale_size(range=c(.1, 1)), package='clusterProfiler', p_size=6, s_x=.98, s_y=.88, s_width=.98, s_height=1.1,h_fill = "#336E7B")
+sticker(p+scale_size(range=c(.1, 1)), package='clusterProfiler', p_size=6, s_x=.98, s_y=.88, s_width=.98, s_height=1.1,h_fill = "#008080", h_color="#336E7B")
