aee0c3eae5f69b9ffafdce23033d8306d5649b5c
jotsetung <johannes.rainer@gmail.com>
2018-02-21 12:27:18 +0100

    Update script for CSAMA2018

--- events/CSAMA/2018/CSAMA2018_lamb.R
+++ events/CSAMA/2018/CSAMA2018_lamb.R
@@ -21,29 +21,9 @@ gg <- ggplot() +
     annotation_custom(img_lamb) +
     theme_void()
 
-col_text <- "#FFFFFF"    ## white
+col_text <- "#000000"    ## white
 col_border <- "#3792AD"  ## BioC blue
-col_bg <- "#696E76"      ## Dark grey
-sticker(gg,
-        package="CSAMA2018",
-        p_size = 7.3,
-        s_x = 1.03,
-        s_y = 0.73,
-        s_width = 2.1,
-        s_height = 1.1,
-        h_fill = col_bg,
-        h_color = col_border,
-        p_family = "Aller_Lt",
-        filename="CSAMA2018_dark.png",
-        spotlight = TRUE,
-        l_x = 1.025, p_color = col_text,
-        url = "www.bioconductor.org",
-        u_color = col_border
-        )
-
-col_text <- "#FFFFFF"    ## white
-col_border <- "#3792AD"  ## BioC blue
-col_bg <- "#ABB7B7"      ## Edward
+col_bg <- "#dfdfe2"
 sticker(gg,
         package="CSAMA2018",
         p_size = 7.3,
@@ -61,23 +41,43 @@ sticker(gg,
         u_color = col_border
         )
 
-col_text <- "#000000"    ## white
-col_border <- "#3792AD"  ## BioC blue
-col_bg <- "#dfdfe2"
-sticker(gg,
-        package="CSAMA2018",
-        p_size = 7.3,
-        s_x = 1.03,
-        s_y = 0.73,
-        s_width = 2.1,
-        s_height = 1.1,
-        h_fill = col_bg,
-        h_color = col_border,
-        p_family = "Aller_Lt",
-        filename="CSAMA2018_light.png",
-        spotlight = TRUE,
-        l_x = 1.025, p_color = col_text,
-        url = "www.bioconductor.org",
-        u_color = col_border
-        )
+## col_text <- "#FFFFFF"    ## white
+## col_border <- "#3792AD"  ## BioC blue
+## col_bg <- "#696E76"      ## Dark grey
+## sticker(gg,
+##         package="CSAMA2018",
+##         p_size = 7.3,
+##         s_x = 1.03,
+##         s_y = 0.73,
+##         s_width = 2.1,
+##         s_height = 1.1,
+##         h_fill = col_bg,
+##         h_color = col_border,
+##         p_family = "Aller_Lt",
+##         filename="CSAMA2018_dark.png",
+##         spotlight = TRUE,
+##         l_x = 1.025, p_color = col_text,
+##         url = "www.bioconductor.org",
+##         u_color = col_border
+##         )
+
+## col_text <- "#FFFFFF"    ## white
+## col_border <- "#3792AD"  ## BioC blue
+## col_bg <- "#ABB7B7"      ## Edward
+## sticker(gg,
+##         package="CSAMA2018",
+##         p_size = 7.3,
+##         s_x = 1.03,
+##         s_y = 0.73,
+##         s_width = 2.1,
+##         s_height = 1.1,
+##         h_fill = col_bg,
+##         h_color = col_border,
+##         p_family = "Aller_Lt",
+##         filename="CSAMA2018_grey.png",
+##         spotlight = TRUE,
+##         l_x = 1.025, p_color = col_text,
+##         url = "www.bioconductor.org",
+##         u_color = col_border
+##         )
 
