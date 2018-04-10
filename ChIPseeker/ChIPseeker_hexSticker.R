cf301d13cac4ebe50ebc9814d33b36a2572ddb13
guangchuang yu <guangchuangyu@gamil.com>
2018-04-10 21:47:18 +0800

    update ChIPseeker

--- ChIPseeker/ChIPseeker_hexSticker.R
+++ ChIPseeker/ChIPseeker_hexSticker.R
@@ -6,6 +6,7 @@ require(hexSticker)
 require(ChIPseeker)
 data("tagMatrixList")
 p <- plotAvgProf(tagMatrixList, xlim=c(-3000, 3000), conf=0.95,resample=500)
-p <- p + theme_void() + ggimage::theme_transparent(legend.position='none')
+p <- p + scale_color_brewer(palette="Set1") + theme_void() +
+    ggimage::theme_transparent(legend.position='none')
 p$layers[[3]] <- NULL
-sticker(p, package="ChIPseeker", p_size=6, s_x=.98, s_y=.88, s_width=1.4, s_height=1.1,h_fill = "#80b1d3", h_color="#3182bd")
+sticker(p, package="ChIPseeker", p_size=6, s_x=.98, s_y=.88, s_width=1.4, s_height=1.1,h_fill = "#114466", h_color="#001030")
